8e7ca4882e94430227241b0187f7398231fbd13e
JuanjoVG <juan.jose.vazquez.gimenez@est.fib.upc.edu>
2018-04-10 20:27:52 +0200

    Add some comments to explains the working of the python files.

--- MultimodalNaiveBayes.py
+++ MultimodalNaiveBayes.py
@@ -5,10 +5,28 @@ import string
 
 class MultimodalNaiveBayes:
     def __init__(self):
-        self.model = {}
         self.targets = []
+        self.model = {}
         self.num_terms = 0
         self.target_terms = {}
+
+    def train(self, train_data):
+        # Init self.targets
+        self.targets = list(set(train_data['Target']))
+
+        # Init self.model
+        self.model = {}
+        for index, row in train_data.iterrows():
+            terms = self.get_terms(row['Text'])
+            for term in terms:
+                self.add_term_to_model(term, row['Target'])
+
+        # Init self.num_terms
+        self.num_terms = len(self.model.keys())
+
+        # Init self.target_terms
+        for target in self.targets:
+            self.target_terms[target] = sum([self.get_occurrences(term, target) for term in self.model.keys()])
 
     def get_terms(self, text):
         clean_text = re.sub('[' + string.punctuation + ']', '', text)
@@ -16,26 +34,30 @@ class MultimodalNaiveBayes:
         terms = clean_text.split()
         return terms
 
-    def train(self, train_data):
-        self.model = {}
-        self.targets = list(set(train_data['Target']))
-        i = 0
-        for index, row in train_data.iterrows():
-            terms = self.get_terms(row['Text'])
-            for term in terms:
-                i += 1
-                self.add_term_to_model(term, row['Target'])
-
-        self.num_terms = len(self.model.keys())
-        for target in self.targets:
-            self.target_terms[target] = sum([self.get_occurrences(term, target) for term in self.model.keys()])
-
     def add_term_to_model(self, term, target):
         if term not in self.model:
             self.model[term] = {}
         if target not in self.model[term]:
             self.model[term][target] = 0
         self.model[term][target] += 1
+
+    def get_occurrences(self, term, target):
+        if term not in self.model:
+            return 0
+        if target not in self.model[term]:
+            return 0
+        return self.model[term][target]
+
+    def test(self, test_data):
+        errors = 0
+        total = 0
+        for index, row in test_data.iterrows():
+            prediction = self.predict(row['Text'])
+            pred_target = max(prediction.items(), key=operator.itemgetter(1))[0]
+            if pred_target != row['Target']:
+                errors += 1
+            total += 1
+        return (errors / total) * 100
 
     def predict(self, text):
         terms = self.get_terms(text)
@@ -47,26 +69,8 @@ class MultimodalNaiveBayes:
             result[target] = target_result
         return result
 
-    def get_occurrences(self, term, target):
-        if term not in self.model:
-            return 0
-        if target not in self.model[term]:
-            return 0
-        return self.model[term][target]
-
     def get_probability(self, target, term):
         return (self.get_occurrences(term, target) + 1) / (self.target_terms[target] + self.num_terms)
-
-    def test(self, test_data):
-        errors = 0
-        total = 0
-        for index, row in test_data.iterrows():
-            prediction = self.predict(row['Text'])
-            pred_target = max(prediction.items(), key=operator.itemgetter(1))[0]
-            if pred_target != row['Target']:
-                errors += 1
-            total += 1
-        return (errors / total)*100
 
     def show(self):
         print("Term probs:")
